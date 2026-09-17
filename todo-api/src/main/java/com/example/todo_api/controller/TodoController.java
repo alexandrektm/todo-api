2d22a5e15cc724c110602cf18c6a3a80ac7cdea9
alexandrektm <alexandrektmitsuuchi@gmail.com>
2026-09-17 07:50:11 -0300

    beginning_rest_control

--- todo-api/src/main/java/com/example/todo_api/controller/TodoController.java
+++ todo-api/src/main/java/com/example/todo_api/controller/TodoController.java
@@ -1,4 +1,29 @@
 package com.example.todo_api.controller;
 
+import com.example.todo_api.model.Todo;
+import com.example.todo_api.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+
+
 }
